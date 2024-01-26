8302f18e707a19c5426f869691b2241555aaabd0
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-01-26 12:31:29 +0100

    first commit

---
+++ app/server.js
@@ -0,0 +1,15 @@
+module.exports =  class Application{
+    constructor(PORT, DB_HOST){
+
+    }
+
+    configApplication(){}
+
+    createServer(PORT){}
+
+    connectToMongoDB(DB_HOST){}
+
+    createRoute(){}
+
+    errorHandler(){}
+}
