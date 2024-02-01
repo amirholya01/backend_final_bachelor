517db41bdc59d2c68a1f6ded8c5cd67be1282af2
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-02-01 11:37:59 +0100

    create a course

--- app/controllers/admin/course.controller.js
+++ app/controllers/admin/course.controller.js
@@ -1,7 +1,10 @@
 const { CourseModel } = require("../../models/courses");
+const { createCourseSchema } = require("../../validations/admin/course.schema");
 const Controller = require("../controller");
 const {StatusCodes: HttpStatus} = require("http-status-codes");
 const path = require("path");
+const createError = require("http-errors");
+
 class CourseController extends Controller{
 
     async getListOfCourse(req, res, next){
@@ -25,10 +28,24 @@ class CourseController extends Controller{
 
     async addCourse(req, res, next){
         try {
+            await createCourseSchema.validateAsync(req.body);
             const{ fileUploadPath, filename} = req.body;
             const image = path.join(fileUploadPath, filename).replace(/\\/g, "/")
             const {title, text, tags, category, price, discount} = req.body;
-            return res.status(HttpStatus.CREATED).json({title, text, tags, category, price, discount, image})
+            const teacher = req.user._id;
+            const course = await CourseModel.create({
+                title,
+                text,
+                tags,
+                category,
+                price,
+                discount,
+                image,
+                time: "00.00.00",
+                teacher 
+            })
+            if(!course?._id) throw createError.InternalServerError("The course was not registered")
+            return res.status(HttpStatus.CREATED).json({})
         } catch (error) {
             next(error);
         }
